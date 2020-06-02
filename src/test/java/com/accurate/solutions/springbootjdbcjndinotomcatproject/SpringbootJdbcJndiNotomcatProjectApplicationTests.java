4ab3fd23bd7c3923907685edc5a3de00e393c25f
Sidagond.Byadagi <Sidagond.X.Byadagi.-ND@disney.com>
2020-06-02 17:04:39 -0400

    Initial commit

---
+++ src/test/java/com/accurate/solutions/springbootjdbcjndinotomcatproject/SpringbootJdbcJndiNotomcatProjectApplicationTests.java
@@ -0,0 +1,12 @@
+package com.accurate.solutions.springbootjdbcjndinotomcatproject;
+
+import org.junit.jupiter.api.Test;
+
+//@SpringBootTest
+class SpringbootJdbcJndiNotomcatProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
